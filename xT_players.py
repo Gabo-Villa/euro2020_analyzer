8f427d19d4c81352b9c09fa6bc238558034cf48b
Gabriel Villarroel <99690843+Gabo-Villa@users.noreply.github.com>
2023-03-05 10:29:54 -0500

    Update xT_players.py

--- xT_players.py
+++ xT_players.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from mplsoccer import Sbopen
 import matplotlib.patches as mpatches
+from mplsoccer import Sbopen
 import os.path
 import streamlit as st
 
